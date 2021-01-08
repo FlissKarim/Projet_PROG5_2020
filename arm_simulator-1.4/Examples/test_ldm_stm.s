77e65f9e1297c048ff8f3aad4f376762967ca552
FlissKarim <75549879+FlissKarim@users.noreply.github.com>
2021-01-08 21:15:29 +0100

    Update test_ldm_stm.s
    test plus pousser

--- arm_simulator-1.4/Examples/test_ldm_stm.s
+++ arm_simulator-1.4/Examples/test_ldm_stm.s
@@ -2,15 +2,23 @@
 .text
 
 main:
-	mov r0, #0
-
+	@ test passed if at the end r5, r6, r7, r8 will have the same values as r1, r2, r3, r4
 	mov r1, #1
 	mov r2, #2
 	mov r3, #3
 	mov r4, #4
 
-	stm r0, {r1, r2, r3, r4}
+	@@@ 1 & 2 do the same thing @@@
 	
-	ldm r0, {r5, r6, r7, r8}
+	@ 1)
+	@ start from the address r0, is the base registre
+	stmia r0, {r1, r2, r3, r4}	
+	ldmia r0, {r5, r6, r7, r8}
+	
+	@ 2)
+	@ start from the address = 12 - 16 (16 is number of registre between brackets * 4) + 4
+	mov r0, #12
+	stmda r0, {r1, r2, r3, r4}	
+	ldmda r0, {r5, r6, r7, r8}
 	
 fin: swi 0x123456
